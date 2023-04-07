eb8f7af6cfd1f6a46e3a8b2b7dacbc619cfb16a3
Lipyeow Lim <81180733+lipyeow-lim@users.noreply.github.com>
2023-04-07 10:23:28 -0700

    Update partitioning fields (#19)
    Co-authored-by: lipyeow-lim <lipyeow.lim@databricks.com>

--- templates/okta_gold.sql
+++ templates/okta_gold.sql
@@ -1,5 +1,5 @@
 CREATE STREAMING LIVE TABLE okta_edges_gold_{{time_granularity}}
-PARTITIONED BY (time_bkt)
+PARTITIONED BY (time_bkt, sub_id)
 TBLPROPERTIES("quality"="gold")
 AS
 SELECT date_trunc('{{time_granularity}}', event_ts) as time_bkt, sub_type, sub_id, sub_name, pred, pred_status, obj_type, obj_id, obj_name,
